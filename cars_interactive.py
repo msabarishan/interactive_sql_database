9573149524860d15de90f8f2b7ab657b959c2914
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-06-21 06:37:41 +0530

    Update cars_interactive.py

--- cars_interactive.py
+++ cars_interactive.py
@@ -1,10 +1,6 @@
-import pandas as pd
-import numpy as np
-import altair as alt
+
 import streamlit as st
-import psycopg2
-import postgreconnect
-from altair import datum
+
 
 st.write("Hi")
 number = st.number_input("Enter number", 0, 25, 6, 2)
