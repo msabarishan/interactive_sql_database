966a1274b61f6a0fd935087cb9807eba5450b015
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-06-21 06:31:50 +0530

    Update cars_interactive.py

--- cars_interactive.py
+++ cars_interactive.py
@@ -6,5 +6,5 @@ import psycopg2
 import postgreconnect
 from altair import datum
 
- st.number_input("label", start, end, default_value, step_size)
- st.number_input("label", start, end, default_value, step_size)
+number = st.number_input("Enter number", 0, 25, 6, 2)
+your_name = st.text_input("Enter your name")
