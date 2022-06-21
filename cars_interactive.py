f5ee18f9dd24669952a637ae450ac888976e58a3
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-06-21 13:44:59 +0530

    Update cars_interactive.py

--- cars_interactive.py
+++ cars_interactive.py
@@ -15,9 +15,8 @@ option = st.selectbox(
 if(option=='delete'):
   st.write('Select the employee Id to be deleted' )
   d = st.number_input("Enter emp number",min_value=1, max_value=1000, step=1)
-  company_data = "DELETE FROM employee  WHERE emp_id = d;"
+  company_data = "DELETE FROM employee  WHERE emp_id = {d};"
   postgreconnect.runquery(company_data)
-  st.write("Success")
     
   
 if(option=='insert'):
