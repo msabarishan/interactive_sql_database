8ce544767df86de6df45f50088be51d4edff88d8
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-06-21 06:35:46 +0530

    Update cars_interactive.py

--- cars_interactive.py
+++ cars_interactive.py
@@ -6,5 +6,6 @@ import psycopg2
 import postgreconnect
 from altair import datum
 
+st.write("Hi")
 number = st.number_input("Enter number", 0, 25, 6, 2)
 your_name = st.text_input("Enter your name")
