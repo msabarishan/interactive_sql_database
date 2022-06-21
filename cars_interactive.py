724d33b0b502d39cedea6c20714c99164c4c44bc
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-06-21 06:53:01 +0530

    Update cars_interactive.py

--- cars_interactive.py
+++ cars_interactive.py
@@ -8,14 +8,14 @@ option = st.selectbox(
   'What would you like to do?', 
   ('update', 'delete', 'insert'))
 
-if(option=='update')
-  st.write('Enter the employee id to be updated)
+if(option=='update'):
+  st.write('Enter the employee id to be updated')
   
-if(option=='delete')
-  st.write('Select the employee Id to be deleted' )
+if(option=='delete'):
+           st.write('Select the employee Id to be deleted' )
   
-if(option=='insert')
-  st.write('Enter the new employee data')
+if(option=='insert'):
+           st.write('Enter the new employee data')
 
 number = st.number_input("Enter number", 0, 25, 6, 2)
 your_name = st.text_input("Enter your name")
