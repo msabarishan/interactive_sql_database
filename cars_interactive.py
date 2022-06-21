c6df5383dea195b546812fb047dbeaaa1c95683c
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-06-21 06:45:53 +0530

    Update cars_interactive.py

--- cars_interactive.py
+++ cars_interactive.py
@@ -3,5 +3,12 @@ import streamlit as st
 
 
 st.write("Hi")
+
+option = st.selectbox(
+  'What would you like to do?', 
+  ('update', 'delete', 'insert'))
+
+st.write('You selected:', option)
+
 number = st.number_input("Enter number", 0, 25, 6, 2)
 your_name = st.text_input("Enter your name")
